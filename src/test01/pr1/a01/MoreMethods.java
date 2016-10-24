fef63bd9ac8a7ab722cd8b1fc4a1beb59b2ed6ed
Amantisia <can.heilgermann@web.de>
2016-10-24 21:04:01 +0200

    more methods and so on

--- src/test01/pr1/a01/MoreMethods.java
+++ src/test01/pr1/a01/MoreMethods.java
@@ -1,6 +1,7 @@
 package test01.pr1.a01;
 
 import java.io.PrintWriter;
+
 /**
  * Created by HalfBaked on 24.10.2016.
  * Aufgabe 1.c
@@ -12,23 +13,26 @@ public class MoreMethods {
 
     public static void main(String[] args) {
         PrintWriter out = new PrintWriter(System.out, true);
-        printAlphabet(out,true);
+        printAlphabet(out, true);
+        printAlphabet(out, false);
     }
-        public static void printAlphabet(PrintWriter out, boolean smallLetters){
-            char s = 'a';
-            char b = 'A';
 
-            while(s <= 'z'){
-                if(smallLetters){
-                    out.print(s);
-                } else {
-                    out.print(b);
-                }
-                out.print(' ');
-                s++;
-                b++;
-                
-            }
+    public static void printAlphabet(PrintWriter out, boolean smallLetters) {
+        int from = 'A';
+        int to = 'Z';
+        int toLower = 32;
+
+        if(smallLetters) {
+            from += toLower;
+            to += toLower;
         }
 
+        String s = "";
+        for(int i = from; i <= to; i++) {
+            s += (char) i + " ";
+        }
+
+        s = s.substring(0, s.length() - 1);
+        out.println(s);
+    }
 }
